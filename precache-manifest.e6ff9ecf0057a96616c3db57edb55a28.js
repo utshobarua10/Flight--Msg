0ee53aa0af5076e646b243e29094187baa104e4b
utshobarua10 <utshobarua10@gmail.com>
2023-11-28 14:24:13 +0600

    Updates

---
+++ precache-manifest.e6ff9ecf0057a96616c3db57edb55a28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b796370c4891343db8ba9212f8094135",
+    "url": "/Flight--Msg/index.html"
+  },
+  {
+    "revision": "c9a524a21ec9980ef62a",
+    "url": "/Flight--Msg/static/css/main.c161c929.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/Flight--Msg/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Flight--Msg/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "c9a524a21ec9980ef62a",
+    "url": "/Flight--Msg/static/js/main.9f71db04.chunk.js"
+  },
+  {
+    "revision": "f089e4271b24ca728d2b",
+    "url": "/Flight--Msg/static/js/runtime~main.17551407.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Flight--Msg/static/media/logo.06e73328.svg"
+  }
+]);
